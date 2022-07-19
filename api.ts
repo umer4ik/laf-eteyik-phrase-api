5804778d281585343a36b8876138a7c1fe0b9810
Umer Hurramov <umer.hurramov@gmail.com>
2022-07-19 19:58:11 +0300

    🎤 Add health/text response

--- api.ts
+++ api.ts
@@ -7,9 +7,13 @@ const { categories, phrases } = await getData()
 serve((req) => {
   const url = new URL(req.url)
   const route = url.pathname.substring('/api/v0'.length)
+  console.log(route)
   switch (route) {
     case '/health': {
       return createResponse({ body: { health: 'healthy' } })
+    }
+    case '/health/text': {
+      return createResponse({ body: JSON.stringify({ health: 'healthy' }), type: 'text' })
     }
     case '/categories': {
       return createResponse({ body: { categories } })
