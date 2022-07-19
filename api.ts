9fd2db175519cc7954b86b7bfd7313b3414301aa
Umer Hurramov <umer.hurramov@gmail.com>
2022-07-19 19:43:21 +0300

    Remove useless tokens

--- api.ts
+++ api.ts
@@ -1,20 +1,12 @@
 import { serve } from "https://deno.land/std@0.148.0/http/server.ts";
 import { getData } from './files.ts'
 import { createResponse } from './response.ts'
-import { checkToken } from './api-tokens.ts'
 
 const { categories, phrases } = await getData()
 
-serve(async (req) => {
-  const token = req.headers.get('api-token')
+serve((req) => {
   const url = new URL(req.url)
   const route = url.pathname.substring('/api/v0'.length)
-  if (!token || (!await checkToken(token) && route !== '/health')) {
-    return createResponse({
-      body: { error: 'bad token' },
-      status: 401
-    })
-  }
   switch (route) {
     case '/health': {
       return createResponse({ body: { health: 'healthy' } })
